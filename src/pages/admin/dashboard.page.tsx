ee5ab5b582910596385bdea9217cf3f3c5c4c656
NFDeap <juniorapf@outlook.com>
2023-08-02 22:41:08 -0300

    style: start admin dash

--- src/pages/admin/dashboard.page.tsx
+++ src/pages/admin/dashboard.page.tsx
@@ -2,13 +2,17 @@ import React, { useState } from 'react';
 
 const dashboard = () => {
 
-    const [openMenu, setOpenMenu] = useState<number | null>(1);
+    const [isActive, setIsActive] = useState(false);
+
+    const handleClick = () => {
+      setIsActive(!isActive);
+    };
 
     return (
         <>
-            <div className="flex h-screen w-full bg-gray-800">
+            <div className="flex h-screen w-full">
                 {/* Start SideBar */}
-                <aside className="w-20 relative z-20 flex-shrink-0 px-2 overflow-y-auto bg-indigo-600 sm:block">
+                <aside className="w-20 relative z-20 flex-shrink-0 px-2 overflow-y-auto sm:block">
                     <div className="mb-6">
                     {/* Start logo */}
                     <div className="flex justify-center">
@@ -24,18 +28,21 @@ const dashboard = () => {
                     {/* Start NavItem */}
                     <div>
                         <ul className="mt-6 leading-10 px-4">
+
                         <li
                             className="mb-3 p-2 rounded-md flex items-center justify-center bg-blue-400 cursor-pointer"
-                            onClick={() => setOpenMenu((prevState) => (prevState !== 1 ? 1 : null))}
+                            onClick={handleClick}
                         >
                             <i className="fas fa-align-left fa-sm text-white"></i>
                         </li>
+
                         <li className="mb-3 p-2 rounded-md flex items-center justify-center bg-pink-400 cursor-pointer">
                             <i className="fas fa-question-circle fa-sm text-white"></i>
                         </li>
                         <li className="mb-3 p-2 rounded-md flex items-center justify-center bg-yellow-400 cursor-pointer">
                             <i className="fas fa-headphones fa-sm text-white"></i>
                         </li>
+
                         <li className="absolute bottom-0 mb-3 p-2 rounded-full flex items-center mx-auto bg-white cursor-pointer">
                             <i className="fas fa-power-off fa-sm text-indigo-600"></i>
                         </li>
@@ -45,16 +52,14 @@ const dashboard = () => {
                     </div>
                 </aside>
                 {/* Start Open Menu */}
-                <aside
-                    className="animate__animated animate__fadeInLeft w-52 relative z-0 flex-shrink-0 hidden px-4 overflow-y-auto bg-gray-100 sm:block"
-                    style={{ display: openMenu === 1 ? 'block' : 'none' }}
-                    onClick={() => setOpenMenu(null)}
+                <aside                    
+                    className={`${isActive ? 'sm:none' : 'sm:block'} animate__animated animate__fadeInLeft w-52 relative z-0 flex-shrink-0 hidden px-4 overflow-y-auto`}
                 >
                     <div className="mb-6">
                     {/* Start Sidebar for open menu */}
                     <div className="grid grid-cols-1 gap-4 grid-cols-2 mt-6">
                         {/* Start Navitem */}
-                        <div className="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
+                        <div className="p-2 flex flex-col items-center rounded-md justify-center shadow-xl cursor-pointer">
                         <div className="rounded-full p-2 bg-indigo-200 flex flex-col items-center">
                             <i className="fas fa-chart-pie fa-sm text-indigo-600"></i>
                         </div>
@@ -62,11 +67,11 @@ const dashboard = () => {
                         </div>
                         {/* End Navitem */}
                         {/* Other Navitems */}
-                        <div className="p-2 flex flex-col items-center bg-white rounded-md justify-center shadow-xl cursor-pointer">
+                        <div className="p-2 flex flex-col items-center rounded-md justify-center shadow-xl cursor-pointer">
                         <div className="rounded-full p-2 bg-indigo-200 flex flex-col items-center">
                             <i className="fas fa-calculator fa-sm text-indigo-600"></i>
                         </div>
-                        <p className="text-xs mt-1 text-center font-semibold">Calculator</p>
+                        <p className="text-xs mt-1 text-center font-semibold">Calculadora</p>
                         </div>
                         {/* ... */}
                     </div>
